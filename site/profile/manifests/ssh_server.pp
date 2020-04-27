58b44b0da66034657bdf8aeb4e043da501e45f0d
cbusby31 <64213338+cbusby31@users.noreply.github.com>
2020-04-27 10:58:54 -0600

    Create ssh_server.pp

---
+++ site/profile/manifests/ssh_server.pp
@@ -0,0 +1,15 @@
+class profile::ssh_server {
+	package {'openssh-server':
+		ensure => present,
+	}
+	service { 'sshd':
+		ensure => 'running',
+		enable => 'true',
+	}
+	ssh_authorized_key { 'root@puppet.local':
+		ensure => present,
+		user   => 'root',
+		type   => 'ssh-rsa',
+		key    => '',
+	}  
+}
