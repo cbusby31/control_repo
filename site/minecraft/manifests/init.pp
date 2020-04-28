30250f71cf3c3bf2dcb10fd259d29b14129cdf0d
cbusby31 <64213338+cbusby31@users.noreply.github.com>
2020-04-28 16:48:56 -0600

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,26 +1,28 @@
-class minecraft {
-  file {'/opt/minecraft':
-   ensure => directory,
+class minecraft (
+  $url = 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.1/minecraft_server.1.12.1.jar',
+  $install_dir = '/opt/minecraft'
+){
+  file { $install_dir:
+    ensure => directory,
   }
-  file {'/opt/minecraft/minecraft_server.jar':
-   ensure => file,
-   source => 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar',
-   before => Service['minecraft'],
+  file { "${install_dir}/minecraft_server.jar":
+    ensure => file,
+    source => $url,
   }
-   package {'java':
-     ensure => present,
-    }
-    file {'/opt/minecraft/eula.txt':
-      ensure => file,
-      content => 'eula=true',
-    }
-   file {'/etc/systemd/system/minecraft.service':
+
+  package {'java':
+    ensure => present,
+  }
+  file {"${install_dir}/eula.txt":
+    ensure => file,
+    content => 'eula=true'
+  }
+  file {'/etc/systemd/system/minecraft.service':
     ensure => file,
     source => 'puppet:///modules/minecraft/minecraft.service',
   }
-  service {'minecraft':
+  service { 'minecraft':
     ensure => running,
     enable => true,
-    require => [Package['java'],File['/opt/minecraft/eula.txt'],File['/etc/systemd/system/minecraft.service']],
   }
 }
