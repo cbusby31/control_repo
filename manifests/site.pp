3ba91f878a8d1dcd626b153e02d0674e3ec92375
cbusby31 <64213338+cbusby31@users.noreply.github.com>
2020-04-28 14:30:24 -0600

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -7,13 +7,12 @@ node default {
     content => "Welcome to ${fqdn}\n",
  }
 }
-node 'minetest.puppet.vm' {
+node 'minetest.puppet.vm': {
   include role::minecraft_server
 }
 node /^web/ {
   include role::app_server
 }
-
 node /^db/ {
   include role::db_server
 }
