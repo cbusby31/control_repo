cd396b5f57c49414133346fb8d12d42f3256113b
cbusby31 <64213338+cbusby31@users.noreply.github.com>
2020-04-29 07:38:46 -0600

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,16 +2,18 @@ node default {
 }
  node 'puppet.local' {
    include role::master_server
-  file { '/root/README':
+   file { '/root/README':
     ensure  => file,
     content => "Welcome to ${fqdn}\n",
  }
 }
-
+node 'minetest.puppet.vm' {
+  include role::minecraft_server
 }
 node /^web/ {
   include role::app_server
 }
+
 node /^db/ {
   include role::db_server
 }
